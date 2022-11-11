848688675c4e13db6eb2a230d00c60ab22b0d79c
Ghost605 <killertrack01@gmail.com>
2022-11-11 11:20:45 +0700

    init source

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import * as THREE from 'three';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
